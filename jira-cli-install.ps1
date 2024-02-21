97898a53256473036e13ec36243599aad13479f3
Yury Golub <yuragolub21@gmail.com>
2024-02-21 22:24:19 +0300

    Update script.

--- jira-cli-install.ps1
+++ jira-cli-install.ps1
@@ -1,39 +1,39 @@
-param(
-    [ValidateRange(-1, 1)]
-    [Int32]
-    $Choice = -1
-)
+$ErrorActionPreference = "Stop"
 
-if (Get-Command -ErrorAction Ignore -Type Application jira) {
+if (Get-Command -ErrorAction Ignore -Type Application jira)
+{
     Write-Warning 'jira-cli already installed'
     return
 }
 
-if ($Choice -eq -1) {
-    $title = "This script will install jira-cli"
-    $question = "How do you want to install it?"
-    $choices = '&All users', '&Current user'
+$title = "This script will install jira-cli"
+$question = "How do you want to install it?"
+$choices = '&All users', '&Current user'
 
-    $Choice = $Host.UI.PromptForChoice($title, $question, $choices, 1)
-}
+$choice = $Host.UI.PromptForChoice($title, $question, $choices, 1)
 
-if ($Choice -eq 0) {
-    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-        $currentShell = [System.AppDomain]::CurrentDomain.FriendlyName
-        Start-Process $currentShell -WorkingDirectory $pwd "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" -Choice ${Choice}" -Verb RunAs
-        exit
+if ($choice -eq 0)
+{
+    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+    {
+        Write-Warning 'You have to run this script as admin'
+        return
     }
 
     $defaultPath = 'c:\Program Files'
-} elseif ($Choice -eq 1) {
+}
+elseif ($choice -eq 1)
+{
     $defaultPath = "${env:USERPROFILE}\AppData\Local\"
 }
 
-if (!($inputPath = Read-Host "Input installation path. Default is [$defaultPath]")) {
+if (!($inputPath = Read-Host "Input installation path. Default is [$defaultPath]"))
+{
     $inputPath = $defaultPath
 }
 
-if (!(Test-Path -Path $inputPath)) {
+if (!(Test-Path -Path $inputPath))
+{
     Write-Warning "Invalid path `'${inputPath}`'"
     return
 }
@@ -46,12 +46,14 @@ $question = 'Are you sure you want to proceed?'
 $choices = '&Yes', '&No'
 
 $proceedDecision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
-if ($proceedDecision -eq 1) {
+if ($proceedDecision -eq 1)
+{
     Write-Host 'Abort'
     return
 }
 
-function Get-RedirectedUrl {
+function Get-RedirectedUrl
+{
     param(
         [Parameter(Mandatory = $true)]
         [uri]$url,
@@ -59,17 +61,20 @@ function Get-RedirectedUrl {
     )
 
     $req = [Net.WebRequest]::CreateDefault($url)
-    if ($referer) {
+    if ($referer)
+    {
         $req.Referer = $referer
     }
 
     $resp = $req.GetResponse()
 
-    if ($resp -and $resp.ResponseUri.OriginalString -ne $url) {
+    if ($resp -and $resp.ResponseUri.OriginalString -ne $url)
+    {
         Write-Verbose "Found redirected url '$($resp.ResponseUri)"
         $result = $resp.ResponseUri.OriginalString
     }
-    else {
+    else
+    {
         Write-Warning "No redirected url was found, returning given url."
         $result = $url
     }
@@ -81,7 +86,8 @@ function Get-RedirectedUrl {
 
 $jiraBin = Join-Path $destPath -ChildPath 'bin'
 
-if (!(Test-Path -Path $jiraBin)) {
+if (!(Test-Path -Path $jiraBin))
+{
     $url = 'https://github.com/ankitpokhrel/jira-cli/releases/latest'
     $redirected = Get-RedirectedUrl $url
 
@@ -89,7 +95,8 @@ if (!(Test-Path -Path $jiraBin)) {
     $version = $tagName.Substring(1)
     $archive = "jira_${version}_windows_x86_64.zip"
 
-    if (!(Test-Path -Path $archive)) {
+    if (!(Test-Path -Path $archive))
+    {
         $downloadUrl = "https://github.com/ankitpokhrel/jira-cli/releases/download/${tagName}/${archive}"
 
         Write-Host "Downloading `'$archive`' from `'${downloadUrl}`'"
@@ -101,32 +108,52 @@ if (!(Test-Path -Path $jiraBin)) {
     Remove-Item $archive
 }
 
-$apiToken = Read-Host "Input api token" -AsSecureString
+if (!($apiToken = Read-Host "Input api token. Press enter to get it from clipboard" -MaskInput))
+{
+    $apiToken = Get-Clipboard
+}
 
-if ($Choice -eq 0) {
+$Env:JIRA_API_TOKEN = $apiToken
+
+if ($choice -eq 0)
+{
     [Environment]::SetEnvironmentVariable('JIRA_API_TOKEN', $apiToken, [EnvironmentVariableTarget]::Machine)
-} elseif ($Choice -eq 1) {
+}
+elseif ($choice -eq 1)
+{
     [Environment]::SetEnvironmentVariable('JIRA_API_TOKEN', $apiToken, [EnvironmentVariableTarget]::User)
 }
 
-$question = "Do you want to add ${jiraBin} to Path?"
+$question = "Do you want to add `'${jiraBin}`' to Path?"
 $choices = '&Yes', '&No'
 
-$addToPath = $Host.UI.PromptForChoice($null, $question, $choices, 1)
-if ($addToPath -eq 0) {
-    if ($Choice -eq 0) {
-        $machinePath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine)
-        if (!($machinePath -split ';' -contains $jiraBin)) {
+if ($choice -eq 0)
+{
+    $machinePath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine)
+    if (!($machinePath -split ';' -contains $jiraBin))
+    {
+        $addToPath = $Host.UI.PromptForChoice($null, $question, $choices, 1)
+        if ($addToPath -eq 0)
+        {
             [Environment]::SetEnvironmentVariable('Path', $machinePath + ";${jiraBin}", [EnvironmentVariableTarget]::Machine)
-        }
-    } elseif ($Choice -eq 1) {
-        $userPath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::User)
-        if (!($userPath -split ';' -contains $jiraBin)) {
-            [Environment]::SetEnvironmentVariable('Path', $userPath + ";${jiraBin}", [EnvironmentVariableTarget]::User)
+
+            $env:Path = [System.Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) + ";" + [System.Environment]::GetEnvironmentVariable('Path',[EnvironmentVariableTarget]::User)
         }
     }
+}
+elseif ($choice -eq 1)
+{
+    $userPath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::User)
+    if (!($userPath -split ';' -contains $jiraBin))
+    {
+        $addToPath = $Host.UI.PromptForChoice($null, $question, $choices, 1)
+        if ($addToPath -eq 0)
+        {
+            [Environment]::SetEnvironmentVariable('Path', $userPath + ";${jiraBin}", [EnvironmentVariableTarget]::User)
 
-    $env:Path = [System.Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) + ";" + [System.Environment]::GetEnvironmentVariable('Path',[EnvironmentVariableTarget]::User)
+            $env:Path = [System.Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) + ";" + [System.Environment]::GetEnvironmentVariable('Path',[EnvironmentVariableTarget]::User)
+        }
+    }
 }
 
 Write-Host 'Installation successfull'
